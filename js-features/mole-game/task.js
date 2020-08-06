c0e8c7be491efbb89d7783e772cb8c142a9ec956
Ilya Tatarinov <itatarinov7@gmail.com>
2020-08-06 19:10:20 +0300

    third task started

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,15 @@
+"use strict"
+
+getHole = index => document.getElementById(`hole${index}`);
+let dead = document.getElementById('dead');
+let lost = document.getElementById('lost');
+
+for (i = 1; i <= 9; i++) {
+    getHole(i).onclick = function() {
+        if (this.className.includes('hole_has-mole')) {
+            dead.textContent = Number(dead.textContent) + 1;
+        } else {
+            lost.textContent = Number(lost.textContent) + 1;
+        }
+    }
+}

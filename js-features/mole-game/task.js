192a0f0a94638d56f26526e0a216e90b98e73cd6
Ilya Tatarinov <itatarinov7@gmail.com>
2020-08-11 18:08:47 +0300

    3 task half done

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -4,12 +4,14 @@ let getHole = index => document.getElementById(`hole${index}`);
 let dead = document.getElementById('dead');
 let lost = document.getElementById('lost');
 
-for (i = 1; i <= 9; i++) {
-    getHole(i).onclick = function() {
+
+for (let i = 1; i <= 9; i++) {   
+      getHole(i).onclick = function() {
         if (this.className.includes('hole_has-mole')) {
             dead.textContent = Number(dead.textContent) + 1;
         } else {
             lost.textContent = Number(lost.textContent) + 1;
-        }
+        } 
     }
 }
+

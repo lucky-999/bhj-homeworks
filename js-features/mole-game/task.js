2ba143181343713bc883217b245d03d91c98b9ce
Ilya Tatarinov <itatarinov7@gmail.com>
2020-08-11 18:22:12 +0300

    mole game task done

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -7,11 +7,24 @@ let lost = document.getElementById('lost');
 
 for (let i = 1; i <= 9; i++) {   
       getHole(i).onclick = function() {
-        if (this.className.includes('hole_has-mole')) {
-            dead.textContent = Number(dead.textContent) + 1;
-        } else {
-            lost.textContent = Number(lost.textContent) + 1;
-        } 
+                if (this.className.includes('hole_has-mole')) {
+                    dead.textContent = Number(dead.textContent) + 1;
+                } else {
+                    lost.textContent = Number(lost.textContent) + 1;
+                }
+                checkWinner(); 
+          }
+        
+    }
+
+function checkWinner() {
+    if (dead.textContent == 10) {
+        alert ('ура, ты выиграл! давай еще!');
+        dead.textContent = 0;
+        lost.textContent = 0;
+    } else if (lost.textContent == 5) {
+        alert ('ты проиграл, давай сначала')
+        dead.textContent = 0;
+        lost.textContent = 0;
     }
 }
-
